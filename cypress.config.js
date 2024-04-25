54235b955d150077a14396e43a75023bdd37b2a2
Fernando <fernandocaiofelberque@gmail.com>
2024-04-25 14:15:26 -0300

    MobileRelatorio

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'yxp32c',
   e2e: {
 
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
